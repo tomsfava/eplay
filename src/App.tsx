1e77fd226ab581d14dc35cd4cbdd6df335ab2c0f
Tomás Fava <tomsfava@gmail.com>
2025-04-21 07:09:22 -0300

    criando o footer e adicionando componente Link ao Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
 import Header from './components/Header'
 import Rotas from './routes'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
       </div>
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }
