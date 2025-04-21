f41d8e843a32d9059b1af0a2b482224f4a0bf8ca
Tomás Fava <tomsfava@gmail.com>
2025-04-21 07:37:56 -0300

    adiciona página de produto no projeto e nas rotas, com parametrização

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Category from './pages/Category'
+import Product from './pages/Product'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categorias" element={<Category />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
